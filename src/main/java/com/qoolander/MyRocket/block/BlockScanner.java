5fdcd5969e86cc5198fe833b3e821f9beb9c95dc
qoolander <sam@toth.co.uk>
2014-07-19 00:12:43 +0100

    Finished doing textures and scanning frames
    Gui next and am working on detecting cubes of frames.

--- src/main/java/com/qoolander/MyRocket/block/BlockScanner.java
+++ src/main/java/com/qoolander/MyRocket/block/BlockScanner.java
@@ -7,12 +7,23 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import javax.swing.Icon;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockChest;
 import net.minecraft.block.BlockFurnace;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
 import scala.reflect.macros.runtime.Names;
 
 import javax.swing.*;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class BlockScanner extends BlockMyRocket{
     public BlockScanner(){
@@ -44,8 +55,62 @@ public class BlockScanner extends BlockMyRocket{
 
     @Override
     @SideOnly(Side.CLIENT)
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase p_149689_5_, ItemStack p_149689_6_)
+    {
+        int l = MathHelper.floor_double((double) (p_149689_5_.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, 4, 2);
+        }
+
+        Block[] blocksAdjacent;
+
+        blocksAdjacent = new Block[6];
+
+
+        blocksAdjacent[0] = world.getBlock(x+1, y,z);
+        blocksAdjacent[1] = world.getBlock(x-1, y,z);
+        blocksAdjacent[2] = world.getBlock(x, y+1,z);
+        blocksAdjacent[3] = world.getBlock(x, y-1,z);
+        blocksAdjacent[4] = world.getBlock(x, y,z+1);
+        blocksAdjacent[5] = world.getBlock(x, y,z-1);
+
+        boolean foundFrame = false;
+
+        for(int f = 0; f<6; f++){
+            if(blocksAdjacent[f] instanceof BlockFrame){
+                foundFrame =true;
+            }
+        }
+        if(foundFrame){
+            System.out.println("Found frame next to scanner. Scanner position x: " +x +", y: " +y +", z: " +z);
+        }else{
+            System.out.println("No frames found by scanner!");
+        }
+
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int metadata) {
         return side == 1 ? this.topIcon : (side == 0 ? this.bottomIcon : (side != metadata ? sideIcon : frontIcon));
     }
+
 
 }
